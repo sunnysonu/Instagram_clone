7db6931b5930f7fc664795f05bd38d3dc9fd4562
sunnysonu <sunnysonu582@gmail.com>
2018-07-14 12:57:04 +0530

    added deploying requirement files

--- insta/settings.py
+++ insta/settings.py
@@ -117,12 +117,31 @@ USE_L10N = True
 
 USE_TZ = True
 
+#-----------------------------------------------------------------
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+#
+# STATIC_URL = '/static/'
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+#
+# MEDIA_URL = "/media/"
 
+#-----------------------------------------------------------------
+
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
-MEDIA_URL = "/media/"
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
